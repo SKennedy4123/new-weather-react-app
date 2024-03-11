e84a2a65b76253b9970271b7bfa8b61f543d47d3
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-11 14:52:27 -0400

    added weather, header, and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import Header from "./Header.js";
+import CurrentWeather from "./CurrentWeather.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <CurrentWeather />
+      <Footer />
     </div>
   );
 }
